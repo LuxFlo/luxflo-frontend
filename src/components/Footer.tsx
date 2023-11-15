149ba874f9712b31b2b4d47458ce26e7a38b7b2d
Julio M Cruz <julio.cruz@eb-ms.net>
2023-11-15 11:35:41 -0500

    Fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="bg-black text-white text-center p-4 animate-fade-in-up">
+    <footer className="bg-black text-white text-center p-4 animate-fade-in-up h-28">
       <p>© {new Date().getFullYear()} LuxFlo. All rights reserved.</p>
     </footer>
   );
