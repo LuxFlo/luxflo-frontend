5b2df8f73c4f2f09f6c61c4a42e6fb75dc3c9c90
tad dy <a@a.com>
2023-11-15 10:20:20 -0500

    fixing build bug

--- src/layouts/dashboardLayout.tsx
+++ src/layouts/dashboardLayout.tsx
@@ -183,7 +183,7 @@ export default function DashboardLayout({
                           </ul>
                         </li>
                         <li className="mt-auto">
-                          <a
+                          <Link
                             href="/dashboard/settings"
                             className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-700 hover:bg-gray-50 hover:text-gray-600"
                           >
@@ -192,7 +192,7 @@ export default function DashboardLayout({
                               aria-hidden="true"
                             />
                             Settings
-                          </a>
+                          </Link>
                         </li>
                       </ul>
                     </nav>
