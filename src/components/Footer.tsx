d13cf892565cbb23e48ad7b3953b5ea2c2d5cb09
patrickbdevaney <79435941+patrickbdevaney@users.noreply.github.com>
2023-11-15 22:02:53 -0500

    added pitch deck link to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,11 @@ import React from "react";
 function Footer() {
   return (
     <footer className="bg-black text-white text-center p-4 animate-fade-in-up h-28">
-      <p>© {new Date().getFullYear()} LuxFlo. All rights reserved.</p>
+      <p>
+        © {new Date().getFullYear()} LuxFlo. All rights reserved.
+       
+      </p>
+      <a href="https://docs.google.com/presentation/d/1SARZXqFIe9KMteMeYkZrcECfA_kiAFfk3XvUYY3511Q/edit?usp=sharing" target="_blank" rel="noreferrer">Pitch Deck</a>
     </footer>
   );
 }
